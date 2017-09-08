fa79bc4ab3d42a999a4bde57976a5041fc6e493f
WINDOWS-Q2MRP0D\aAvishkar <avishkar7890@gmail.com>
2017-09-08 17:07:39 +0530

    Updates to write subscription to file

--- subscription-service-web/src/main/java/com/mad/subscription/service/controller/SubscriptionController.java
+++ subscription-service-web/src/main/java/com/mad/subscription/service/controller/SubscriptionController.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.io.FileUtils;
 
@@ -15,14 +18,14 @@ public class SubscriptionController {
 	 
 	@GET
 	@Path("/{clientId}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getClientId(@PathParam("clientId") String name) {
-		String output = "Welcome   : " + name;
 		try {
-			FileUtils.writeStringToFile(new File("\\var\\subscription.txt"), name, true);
+			FileUtils.writeStringToFile(new File("\\var\\subscription.txt"), name + "\r\n", true);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	    return Response.status(200).entity(output).build();
+	    return Response.status(200).entity("OK").build();
 	}
 
 }
